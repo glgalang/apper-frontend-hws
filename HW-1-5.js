c308b8ea461051fa065643e3d7b968bcd88a3691
glgalang <louieangelo.galang@gcash.com>
2022-03-27 01:31:22 +0800

    convert to vowel added conditions

--- HW-1-5.js
+++ HW-1-5.js
@@ -4,12 +4,17 @@
 const convertToVowels = (strng) => {
     let newWord = '';
     let vowels = "aeiou";
-
+    if (typeof(strng) === "string") {
     for(let i = 0;i< strng.length; i++){
         if(vowels.indexOf(strng[i].toLowerCase()) > -1){
             newWord += strng[i];
         }
+      }
+    } else {
+     console.log(`Not a valid input`)
     }
-    return newWord;
+      return newWord;
 }
 console.log(convertToVowels("Hello World"))
+console.log(convertToVowels("The qUick BrowN fOx jumps over the Lazy DOg12"))
+console.log(convertToVowels(123123))
